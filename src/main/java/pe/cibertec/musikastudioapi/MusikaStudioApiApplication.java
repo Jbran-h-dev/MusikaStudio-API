1ad66f09eef8e31fa602e2fd42e330f96ed3a868
Jhabel <emprendeconmilan@gmail.com>
2025-08-24 11:30:59 -0500

    init

---
+++ src/main/java/pe/cibertec/musikastudioapi/MusikaStudioApiApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec.musikastudioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusikaStudioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusikaStudioApiApplication.class, args);
+    }
+
+}
